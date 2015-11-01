cf93f3d614bc0ae79e069382b8101d9e4e8d15d1
Huynk <adukhuyet@users.noreply.github.com>
2015-11-01 15:07:09 +0700

    Việt hóa by HuyNK

--- MasterSharp/LXOrbwalker.cs
+++ MasterSharp/LXOrbwalker.cs
@@ -83,86 +83,86 @@ namespace MasterSharp
 
             Menu = menu;
 
-            var menuDrawing = new Menu("Drawing", "lxOrbwalker_Draw");
+            var menuDrawing = new Menu("Hiển Thị", "lxOrbwalker_Draw");
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_AARange", "AA Circle").SetValue(new Circle(true, Color.FloralWhite)));
+                new MenuItem("lxOrbwalker_Draw_AARange", "Tầm đánh ").SetValue(new Circle(true, Color.FloralWhite)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_AARange_Enemy", "AA Circle Enemy").SetValue(new Circle(true, Color.Pink)));
+                new MenuItem("lxOrbwalker_Draw_AARange_Enemy", "Tầm đánh của địch").SetValue(new Circle(true, Color.Pink)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_Holdzone", "Holdzone").SetValue(new Circle(true, Color.FloralWhite)));
+                new MenuItem("lxOrbwalker_Draw_Holdzone", "Giữ vị trí").SetValue(new Circle(true, Color.FloralWhite)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_MinionHPBar", "Minion HPBar").SetValue(new Circle(true, Color.Black)));
+                new MenuItem("lxOrbwalker_Draw_MinionHPBar", "Thanh máu lính").SetValue(new Circle(true, Color.Black)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_MinionHPBar_thickness", "^ HPBar Thickness").SetValue(new Slider(1, 1, 3)));
+                new MenuItem("lxOrbwalker_Draw_MinionHPBar_thickness", "^ Chế độ hiển thị").SetValue(new Slider(1, 1, 3)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_hitbox", "Show HitBoxes").SetValue(new Circle(true, Color.FloralWhite)));
+                new MenuItem("lxOrbwalker_Draw_hitbox", "Vòng tròn lính").SetValue(new Circle(true, Color.FloralWhite)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_Lasthit", "Minion Lasthit").SetValue(new Circle(true, Color.Lime)));
+                new MenuItem("lxOrbwalker_Draw_Lasthit", "Lính LastHit").SetValue(new Circle(true, Color.Lime)));
             menuDrawing.AddItem(
-                new MenuItem("lxOrbwalker_Draw_nearKill", "Minion nearKill").SetValue(new Circle(true, Color.Gold)));
+                new MenuItem("lxOrbwalker_Draw_nearKill", "Lính gần chết").SetValue(new Circle(true, Color.Gold)));
             menu.AddSubMenu(menuDrawing);
 
             var menuMisc = new Menu("Misc", "lxOrbwalker_Misc");
-            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_Holdzone", "Hold Position").SetValue(new Slider(50, 100, 0)));
-            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_Farmdelay", "Farm Delay").SetValue(new Slider(0, 200, 0)));
+            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_Holdzone", "Giữ vị trí").SetValue(new Slider(50, 100, 0)));
+            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_Farmdelay", "Độ trể farm").SetValue(new Slider(0, 200, 0)));
             menuMisc.AddItem(
-                new MenuItem("lxOrbwalker_Misc_ExtraWindUp", "Extra Winduptime").SetValue(new Slider(80, 200, 0)));
-            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_AutoWindUp", "Autoset Windup").SetValue(false));
+                new MenuItem("lxOrbwalker_Misc_ExtraWindUp", "Đỗ trể khi AA và di chuyển").SetValue(new Slider(80, 200, 0)));
+            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_AutoWindUp", "Tự động chỉnh windump").SetValue(false));
             menuMisc.AddItem(
-                new MenuItem("lxOrbwalker_Misc_Priority_Unit", "Priority Unit").SetValue(
+                new MenuItem("lxOrbwalker_Misc_Priority_Unit", "Chế độ ưu tiên").SetValue(
                     new StringList(new[] {"Minion", "Hero"})));
             menuMisc.AddItem(
                 new MenuItem("lxOrbwalker_Misc_Humanizer", "Humanizer Delay").SetValue(new Slider(50, 100, 0)));
-            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_AllMovementDisabled", "Disable All Movement").SetValue(false));
-            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_AllAttackDisabled", "Disable All Attacks").SetValue(false));
+            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_AllMovementDisabled", "Tắt tât cả các di chuyển").SetValue(false));
+            menuMisc.AddItem(new MenuItem("lxOrbwalker_Misc_AllAttackDisabled", "Tắt tự động đánh").SetValue(false));
 
             menu.AddSubMenu(menuMisc);
 
             var menuMelee = new Menu("Melee", "lxOrbwalker_Melee");
-            menuMelee.AddItem(new MenuItem("lxOrbwalker_Melee_Prediction", "Movement Prediction").SetValue(false));
+            menuMelee.AddItem(new MenuItem("lxOrbwalker_Melee_Prediction", "Đoán hướng di chuyển").SetValue(false));
             menu.AddSubMenu(menuMelee);
 
             var menuModes = new Menu("Orbwalk Mode", "lxOrbwalker_Modes");
             {
-                var modeCombo = new Menu("Combo", "lxOrbwalker_Modes_Combo");
-                modeCombo.AddItem(new MenuItem("Combo_Key", "Key").SetValue(new KeyBind(32, KeyBindType.Press)));
-                modeCombo.AddItem(new MenuItem("Combo_move", "Movement").SetValue(true));
-                modeCombo.AddItem(new MenuItem("Combo_attack", "Attack").SetValue(true));
-                modeCombo.AddItem(new MenuItem("Move_target", "Move to target").SetValue(true));
+                var modeCombo = new Menu("Chiến ", "lxOrbwalker_Modes_Combo");
+                modeCombo.AddItem(new MenuItem("Combo_Key", "Phím").SetValue(new KeyBind(32, KeyBindType.Press)));
+                modeCombo.AddItem(new MenuItem("Combo_move", "Di chuyển").SetValue(true));
+                modeCombo.AddItem(new MenuItem("Combo_attack", "Tấn công").SetValue(true));
+                modeCombo.AddItem(new MenuItem("Move_target", "Di chuyển đến mục tiêu").SetValue(true));
                 menuModes.AddSubMenu(modeCombo);
 
-                var modeHarass = new Menu("Harass", "lxOrbwalker_Modes_Harass");
+                var modeHarass = new Menu("Cấu rỉa", "lxOrbwalker_Modes_Harass");
                 modeHarass.AddItem(
-                    new MenuItem("Harass_Key", "Key").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
-                modeHarass.AddItem(new MenuItem("Harass_move", "Movement").SetValue(true));
-                modeHarass.AddItem(new MenuItem("Harass_attack", "Attack").SetValue(true));
-                modeHarass.AddItem(new MenuItem("Harass_Lasthit", "Lasthit Minions").SetValue(true));
+                    new MenuItem("Harass_Key", "Phím").SetValue(new KeyBind("C".ToCharArray()[0], KeyBindType.Press)));
+                modeHarass.AddItem(new MenuItem("Harass_move", "Di chuyển").SetValue(true));
+                modeHarass.AddItem(new MenuItem("Harass_attack", "Tấn công").SetValue(true));
+                modeHarass.AddItem(new MenuItem("Harass_Lasthit", " Ưu tiên lasthit lính").SetValue(true));
                 menuModes.AddSubMenu(modeHarass);
 
-                var modeLaneClear = new Menu("LaneClear", "lxOrbwalker_Modes_LaneClear");
+                var modeLaneClear = new Menu("Đẩy đường", "lxOrbwalker_Modes_LaneClear");
                 modeLaneClear.AddItem(
                     new MenuItem("LaneClear_Key", "Key").SetValue(new KeyBind("V".ToCharArray()[0], KeyBindType.Press)));
-                modeLaneClear.AddItem(new MenuItem("LaneClear_move", "Movement").SetValue(true));
-                modeLaneClear.AddItem(new MenuItem("LaneClear_attack", "Attack").SetValue(true));
+                modeLaneClear.AddItem(new MenuItem("LaneClear_move", "Di chuyển").SetValue(true));
+                modeLaneClear.AddItem(new MenuItem("LaneClear_attack", "Tấn công").SetValue(true));
                 menuModes.AddSubMenu(modeLaneClear);
 
-                var modeLaneFreeze = new Menu("LaneFreeze", "lxOrbwalker_Modes_LaneFreeze");
+                var modeLaneFreeze = new Menu("Đóng băng lính", "lxOrbwalker_Modes_LaneFreeze");
                 modeLaneFreeze.AddItem(
-                    new MenuItem("LaneFreeze_Key", "Key").SetValue(new KeyBind("Z".ToCharArray()[0], KeyBindType.Press)));
-                modeLaneFreeze.AddItem(new MenuItem("LaneFreeze_move", "Movement").SetValue(true));
-                modeLaneFreeze.AddItem(new MenuItem("LaneFreeze_attack", "Attack").SetValue(true));
+                    new MenuItem("LaneFreeze_Key", "Phím").SetValue(new KeyBind("Z".ToCharArray()[0], KeyBindType.Press)));
+                modeLaneFreeze.AddItem(new MenuItem("LaneFreeze_move", "Di chuyển").SetValue(true));
+                modeLaneFreeze.AddItem(new MenuItem("LaneFreeze_attack", "tấn công").SetValue(true));
                 menuModes.AddSubMenu(modeLaneFreeze);
 
                 var modeLasthit = new Menu("LastHit", "lxOrbwalker_Modes_LastHit");
                 modeLasthit.AddItem(
                     new MenuItem("LastHit_Key", "Key").SetValue(new KeyBind("X".ToCharArray()[0], KeyBindType.Press)));
-                modeLasthit.AddItem(new MenuItem("LastHit_move", "Movement").SetValue(true));
-                modeLasthit.AddItem(new MenuItem("LastHit_attack", "Attack").SetValue(true));
+                modeLasthit.AddItem(new MenuItem("LastHit_move", "Di chuyển").SetValue(true));
+                modeLasthit.AddItem(new MenuItem("LastHit_attack", "Tấn Công").SetValue(true));
                 menuModes.AddSubMenu(modeLasthit);
 
-                var modeFlee = new Menu("Flee", "lxOrbwalker_Modes_Flee");
+                var modeFlee = new Menu("Chạy trốn", "lxOrbwalker_Modes_Flee");
                 modeFlee.AddItem(
-                    new MenuItem("Flee_Key", "Key").SetValue(new KeyBind("A".ToCharArray()[0], KeyBindType.Press)));
+                    new MenuItem("Flee_Key", "Phím").SetValue(new KeyBind("G".ToCharArray()[0], KeyBindType.Press)));
                 menuModes.AddSubMenu(modeFlee);
             }
             menu.AddSubMenu(menuModes);
